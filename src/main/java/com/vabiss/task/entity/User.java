d0b33dc6329599987be93933860b750130853535
ulvu <ulvu.suleyman.00@gmail.com>
2021-10-24 12:23:20 +0400

    Add liquibase migrations

--- src/main/java/com/vabiss/task/entity/User.java
+++ src/main/java/com/vabiss/task/entity/User.java
@@ -4,14 +4,12 @@ import com.vabiss.task.model.request.SignUpRequestModel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @Data
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
